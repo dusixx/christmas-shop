7213408845a7b787d7a25330b8a3745417b7acbe
dusixx <113414935+dusixx@users.noreply.github.com>
2026-04-15 07:34:10 +0300

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src",
-  base: "/",
+  base: "./",
   build: {
+    outDir: "../dist",
     assetsDir: "",
-    minify: true,
+    minify: false,
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, "src/index.html"),
+        gifts: resolve(import.meta.dirname, "src/gifts.html"),
+      },
+    },
   },
 });
